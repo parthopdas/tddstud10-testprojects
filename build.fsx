a693b03ffed0edbdfa378325e3251856501a682f
Partho P. Das <this@parthopdas.com>
2016-08-10 11:16:58 +1000

    Building all projects

--- build.fsx
+++ build.fsx
@@ -2,9 +2,24 @@
 #r "./packages/FAKE/tools/FakeLib.dll"
 
 open Fake
+open System
+open System.IO
+
+let buildOne cmd =
+    async {
+        return cmd
+    }
 
 // Targets
-Target "BuildAll" (fun _ -> ()
+Target "BuildAll" (fun _ ->
+    !! "**/build.cmd"
+    |> Seq.filter (fun it -> not <| it.Equals(Path.GetFullPath("./build.cmd"), StringComparison.OrdinalIgnoreCase))
+    |> Seq.map (fun it -> Shell.AsyncExec(it, "", Path.GetDirectoryName(it)))
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> Array.map (fun it -> if it = 0 then true else false)
+    |> Array.fold (&&) true
+    |> fun it -> if not it then failwith "One or more of the child builds failed"
 )
 
 // start build
